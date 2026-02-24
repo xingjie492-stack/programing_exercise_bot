6739f8d7fb466bbc61902033dc8ad6937bc40842
xingjie492-stack <xingjie492@gmail.com>
2026-02-24 20:56:49 +0900

    geminiの設定絡みのプログラムをpresentation/views.pyに記述。

--- presentation/views.py
+++ presentation/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models import db, User, Submissions
 from flask_login import login_required
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
 presentation_bp = Blueprint(
     'presentation', 
@@ -8,7 +11,16 @@ presentation_bp = Blueprint(
     url_prefix='/presentation'
     )
 
-@presentation_bp.route("/")
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+if not GOOGLE_API_KEY:
+    raise ValueError("GOOGLE_API_KEY is not set in the environment variables.")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-2.5-flash-lite')
+
+@presentation_bp.route("/presentation")
 @login_required
 def presentation():
     return render_template("presentation/presentation.html")
