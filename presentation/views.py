f4eb1787e87e870c73a10332fa0ad3b8e08550b0
xingjie492-stack <xingjie492@gmail.com>
2026-02-25 18:06:20 +0900

    upload.htmlを編集して、出してもらったお題を表示する機能を実装。それに伴い、base.htmlにheaderブロックを追加。

--- presentation/views.py
+++ presentation/views.py
@@ -35,7 +35,7 @@ def generate_content(prompt):
 @login_required
 def generate_problem():
     prompt = (
-    """あなたはpythonの教師です。python初学者向けの教科書は一通り読んだという生徒に対して、その実力を試せるコーディングのお題を出題してください。
+    """あなたはpythonの教師です。python初学者向けの教科書は一通り読んだという生徒に対して、その実力を試せるコーディングのお題をテキストベースで出題してください。なお、お題はMarkdown形式で出力してください。
     """
     )
     problem = generate_content(prompt)
